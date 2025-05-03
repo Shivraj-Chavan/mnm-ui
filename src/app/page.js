eb2515bb0ac7032df5f36b21f0118d89f16ab37c
Shivraj Chavan <shivrajchavan1902@gmail.com>
2025-05-03 23:57:29 +0530

    launch date added

--- src/app/page.js
+++ src/app/page.js
@@ -5,8 +5,10 @@ export default function Home() {
   return (
     <div>
       <main className="flex-grow flex flex-col justify-center items-center text-center">
+        {/* <p className="text-md font-bold py-5 text-orange-500">We’re working hard to bring you something amazing</p> */}
+        <h2 className="text-2xl font-bold py-5 text-orange-500">Launching on <span className="text-3xl text-blue-700" >12 july 2025</span> Stay tuned!</h2>
         {/* <Image src="/logo.png" alt="logo" height={120} width={280} /> */}
-        <h1 className="text-4xl font-bold py-5 text-orange-500">
+        <h1 className="text-2xl font-bold py-5 text-orange-500">
           Registration in Progress...
         </h1>
         <Link
@@ -14,7 +16,8 @@ export default function Home() {
           href="/contactus"
         >
          Click here to Inquire 
-        </Link><Link
+        </Link>
+        <Link
           className="bg-green-700 mt-7 p-3 px-6 rounded-lg text-white text-lg hover:bg-green-800 transition"
           href="/register"
         >
