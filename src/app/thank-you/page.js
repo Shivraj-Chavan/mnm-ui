bcc4876b88d747de581f111bfe5aa44fce93276f
Shivraj Chavan <shivrajchavan1902@gmail.com>
2025-05-14 23:47:43 +0530

    scanner styling added

--- src/app/thank-you/page.js
+++ src/app/thank-you/page.js
@@ -5,8 +5,8 @@ export default function page() {
   return (
     <div className='flex justify-center items-center'>
         <div className='flex flex-col  items-center'>
-        <Image alt="Scanner image"  src='/images/paymentScanner.jpeg' height='300' width='300' />
-        <h2 className='py-2 font-semibold text-md'>Send screenshot after successful payment on 9152418196</h2>
+            <Image alt="Scanner image"  src='/images/paymentScanner.jpeg' height='300' width='300' />
+            <h2 className='py-2 px-3 font-semibold text-md text-center'>Send screenshot after successful payment on 9152418196</h2>
         </div>
     </div>
   )
